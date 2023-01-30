e30dd8bc2c5c31b82c57486690325db6fe578a6d
Liana Akobian <lianaakobian@gmail.com>
2023-01-30 22:05:13 +0100

    adding outputs dir

--- sequential_training.sh
+++ sequential_training.sh
@@ -2,7 +2,7 @@
 
 echo "____MDFEND training____"
 for i in {1..10}; do
-    python main.py --model_name mdfend --batchsize 33 --lr 0.0007 --train './data/weibo21/train$i.pkl' --val './data/weibo21/val$i.pkl' --test './data/weibo21/test$i.pkl'> "mdfend_split$i.txt"  
+    python main.py --model_name mdfend --batchsize 33 --lr 0.0007 --train ./data/weibo21/train$i.pkl --val ./data/weibo21/val$i.pkl --test ./data/weibo21/test$i.pkl > ./data/outputs/mdfend_split$i.txt  
     if [ $? -eq 0 ]; then
         echo "Training completed successfully"
     else
@@ -14,7 +14,7 @@ done
 
 echo "____BERT_all training____"
 for i in {1..10}; do
-    python main.py --model_name bert_all --batchsize 33 --lr 0.0007 --train './data/weibo21/train$i.pkl' --val './data/weibo21/val$i.pkl' --test './data/weibo21/test$i.pkl'> "bert_all_split$i.txt"  
+    python main.py --model_name bert_all --batchsize 33 --lr 0.0007 --train ./data/weibo21/train$i.pkl --val ./data/weibo21/val$i.pkl --test ./data/weibo21/test$i.pkl> ./data/outputs/bert_all_split$i.txt  
     if [ $? -eq 0 ]; then
         echo "Training completed successfully"
     else
@@ -27,7 +27,7 @@ done
 echo "____BERT_single training____"
 for i in {1..10}; do
     for c in {0..8}; do
-        python main.py --model_name bert_single --category $c --batchsize 33 --lr 0.0007 --train './data/weibo21/train$i.pkl' --val './data/weibo21/val$i.pkl' --test './data/weibo21/test$i.pkl'> "bert_single_split$i_cat$c.txt"  
+        python main.py --model_name bert_single --category $c --batchsize 33 --lr 0.0007 --train ./data/weibo21/train$i.pkl --val ./data/weibo21/val$i.pkl --test ./data/weibo21/test$i.pkl> ./data/outputs/bert_single_split$i_cat$c.txt 
         if [ $? -eq 0 ]; then
             echo "Training completed successfully"
         else
@@ -40,7 +40,7 @@ done
 
 echo "____TextCNN_all training____"
 for i in {1..10}; do
-    python main.py --model_name textcnn_all --emb_type w2v --batchsize 33 --lr 0.0007 --train './data/weibo21/train$i.pkl' --val './data/weibo21/val$i.pkl' --test './data/weibo21/test$i.pkl'> "textcnn_all_split$i.txt"  
+    python main.py --model_name textcnn_all --emb_type w2v --batchsize 33 --lr 0.0007 --train ./data/weibo21/train$i.pkl --val ./data/weibo21/val$i.pkl --test ./data/weibo21/test$i.pkl> ./data/outputs/textcnn_all_split$i.txt  
     if [ $? -eq 0 ]; then
         echo "Training completed successfully"
     else
@@ -53,7 +53,7 @@ done
 echo "____TextCNN_single training____"
 for i in {1..10}; do
     for c in {0..8}; do
-        python main.py --model_name textcnn_single --emb_type w2v --category $c --batchsize 33 --lr 0.0007 --train './data/weibo21/train$i.pkl' --val './data/weibo21/val$i.pkl' --test './data/weibo21/test$i.pkl'> "textcnn_single_split$i_cat$c.txt"  
+        python main.py --model_name textcnn_single --emb_type w2v --category $c --batchsize 33 --lr 0.0007 --train ./data/weibo21/train$i.pkl --val ./data/weibo21/val$i.pkl --test ./data/weibo21/test$i.pkl> ./data/outputs/textcnn_single_split$i_cat$c.txt  
         if [ $? -eq 0 ]; then
             echo "Training completed successfully"
         else
